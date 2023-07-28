d88b97baf032611d8e2551b18d92c12847e3bd62
aryabokon <aryabokon@dpkapp.ru>
2023-07-28 13:12:56 +0300

    fix test_get_character

--- src/tests/get_character_test.py
+++ src/tests/get_character_test.py
@@ -10,4 +10,4 @@ def test_get_character(api_characters: ApiCharacter, reset_system_state):
     #  Check that this hero return by request
     get_hero = api_characters.get_character(our_hero["name"]).json()["result"]
     for key_out, key_in in zip(our_hero, get_hero):
-        assert key_in == key_out
+        assert get_hero[key_in] == our_hero[key_out]
